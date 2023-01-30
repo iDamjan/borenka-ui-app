34c05b02e6ff2bece25a88bf9737a4afc3c1dec9
Damjan Stojanovski <damjan.stojanovski123@gmail.com>
2023-01-30 19:24:29 +0100

    fix

---
+++ src/components/Preload/Preload.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import classes from './Preload.module.scss'
+export const Preload = () => {
+  return (
+    <div className={classes.Preload}>
+        <h1>Hello there</h1>
+    </div>
+  )
+}
