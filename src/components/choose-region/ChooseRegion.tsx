8162fd7b0cd18ab374f7871b329beb2f803a193e
Damjan Stojanovski <damjan.stojanovski123@gmail.com>
2023-02-05 17:41:11 +0100

    media queries added for multiple breakpoints on map page

--- src/components/choose-region/ChooseRegion.tsx
+++ src/components/choose-region/ChooseRegion.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, Suspense } from "react";
 import Map from "./map/Map";
 import Logo from "../../static/Logo.svg";
 import classes from "./choose-region.module.scss";
@@ -14,19 +14,22 @@ export const ChooseRegion = () => {
 
   return (
     <div className={classes.container}>
-      
-      <div className={classes["container__header"]}>
-        <img src={Logo} alt="logo" onClick={() => navigate("/home")} />
-        <MobileDrawer />
+      <Suspense fallback={<div>loading...</div>}>
+        <div className={classes["container__header"]}>
+          <img src={Logo} alt="logo" onClick={() => navigate("/home")} />
+          <MobileDrawer />
 
-        <KeyboardReturnIcon
-        className={classes.backNavigation}
-        onClick={() => navigate("/home")}
-      />
-        <h1>{currentHoveredRegion}</h1>
-      </div>
-      <Map setCurrentHoveredRegion = {setCurrentHoveredRegion}/>
-      <h1 className={classes.mobileHeading}>Избери регион и посади виртуелно дрво</h1>
+          <KeyboardReturnIcon
+            className={classes.backNavigation}
+            onClick={() => navigate("/home")}
+          />
+          <h1>{currentHoveredRegion}</h1>
+        </div>
+        <Map setCurrentHoveredRegion={setCurrentHoveredRegion} />
+        <h1 className={classes.mobileHeading}>
+          Избери регион и посади виртуелно дрво
+        </h1>
+      </Suspense>
     </div>
   );
 };
