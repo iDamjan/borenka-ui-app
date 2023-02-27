25d9dcaa5045a4c35a04570de4e5e21318f85e17
iDamjan <damjan.stojanovski123@gmail.com>
2023-02-27 21:13:00 +0100

    update

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <App />
     </Router>
   </React.StrictMode>
